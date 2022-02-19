65071fd7c97105b4aebe1c92c69b93eeede2fff4
Adnan <adnan.peker2@gmail.com>
2022-02-19 09:59:12 +0200

    Initial commit

---
+++ Commands/Annoucement.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+
+namespace mei.Commands
+{
+
+    public class Announcements : BaseCommandModule
+    {   
+        [Command("yaz"), Aliases("write","wrote","yazdır","say"), Description("Bota istediğinizi yazdırır."), RequirePermissions(DSharpPlus.Permissions.Administrator)]
+        public async Task GreetCommand(CommandContext ctx, [RemainingText] string name)
+        {
+            await ctx.Message.DeleteAsync();
+            await ctx.Message.Channel.SendMessageAsync($"{name}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using mei.Commands;
+
+namespace mei
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MainAsync().GetAwaiter().GetResult();
+        }
+        static async Task MainAsync()
+        {
+            var discord = new DiscordClient(new DiscordConfiguration()
+            {
+                Token = "type your token",
+                TokenType = TokenType.Bot,
+                AutoReconnect = true,
+            });
+
+           
+            var commands = discord.UseCommandsNext(new CommandsNextConfiguration()
+            {
+                StringPrefixes = new[] { "!" }
+            });
+
+            commands.RegisterCommands < Announcements > ();
+
+
+            discord.MessageCreated += async (s, e) =>
+            {
+                if (e.Message.Content.ToLower() == "selam")
+                {
+                    await e.Message.RespondAsync("Selam!");
+                }
+                else if(e.Message.Content.ToLower() == "ping")
+                {
+                    await e.Message.RespondAsync("pong!");
+                }
+            };
+
+            await discord.ConnectAsync();
+            await Task.Delay(-1);
+            Console.WriteLine("Bot akitf edildi.");
+        }
+    }
+}
