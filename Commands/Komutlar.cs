1128c6d987b1281be03bfe302fb8638284b7f516
Adnan <adnan.peker2@gmail.com>
2022-02-19 15:38:22 +0200

    Avatar komutu eklendi

---
+++ Commands/Komutlar.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.Entities;
+using DSharpPlus.Interactivity.Extensions;
+
+namespace meichan.Commands
+{
+
+    public class Komutlar : BaseCommandModule
+    {
+        [Command("yaz"), Aliases("write", "wrote", "yazdır", "say"), Description("Bota istediğinizi yazdırır."), RequirePermissions(DSharpPlus.Permissions.Administrator)]
+        public async Task GreetCommand(CommandContext ctx, [RemainingText] string name)
+        {
+            await ctx.Message.DeleteAsync();
+            await ctx.Message.Channel.SendMessageAsync($"{name}");
+        }
+
+        [Command("avatar"), Description("Kendinizin ya da bir başkasının profil fotoğrafını görmenize yarar.")]
+        public async Task Avatar(CommandContext ctx, DiscordUser user = null)
+        {
+            if (user == null)
+            {
+                var embed = new DiscordEmbedBuilder
+                {
+                    ImageUrl = $"{ctx.Member.AvatarUrl}",
+                    Timestamp = DateTime.UtcNow,
+                };
+                var msg = await ctx.Channel.SendMessageAsync(embed);
+            }
+            else
+            {
+                var embed = new DiscordEmbedBuilder
+                {
+                    ImageUrl = $"{user.AvatarUrl}",
+                    Timestamp = DateTime.UtcNow,
+                    Footer = new DiscordEmbedBuilder.EmbedFooter
+                    {
+                        Text = $"{ctx.Member.Username}",
+                        IconUrl = $"{ctx.Member.AvatarUrl}"
+                    },
+                };
+                var msg = await ctx.Channel.SendMessageAsync(embed);
+            }
+        }
+    }
+}
