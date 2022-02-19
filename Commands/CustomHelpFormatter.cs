43796d2de1655df59c6e8ba58186e2fa8d40746c
Adnan <adnan.peker2@gmail.com>
2022-02-20 01:16:14 +0200

    Help komutu görünümü değişti

---
+++ Commands/CustomHelpFormatter.cs
@@ -0,0 +1,57 @@
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Converters;
+using DSharpPlus.CommandsNext.Entities;
+using DSharpPlus.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mei.Commands
+{
+    public class CustomHelpFormatter : BaseHelpFormatter
+    {
+        protected DiscordEmbedBuilder _embed;
+        protected StringBuilder _strBuilder;
+
+        public CustomHelpFormatter(CommandContext ctx) : base(ctx)
+        {
+            _embed = new DiscordEmbedBuilder();
+            _strBuilder = new StringBuilder();
+
+            // Help formatters do support dependency injection.
+            // Any required services can be specified by declaring constructor parameters. 
+
+            // Other required initialization here ...
+        }
+
+        public override BaseHelpFormatter WithCommand(Command command)
+        {
+            _embed.AddField("!" + command.Name + " komutu nedir?", command.Description);
+            _embed.WithColor(DiscordColor.Rose);
+            _strBuilder.AppendLine("!" + $"{command.Name} - {command.Description}");
+
+            return this;
+        }
+
+        public override BaseHelpFormatter WithSubcommands(IEnumerable<Command> cmds)
+        {
+            foreach (var cmd in cmds)
+            {
+                _embed.AddField("!" + cmd.Name, cmd.Description);
+                _embed.WithColor(DiscordColor.Rose);
+                _embed.WithTitle("Komutlar");
+                _strBuilder.AppendLine($"{cmd.Name} - {cmd.Description}");
+            }
+
+            return this;
+        }
+
+        public override CommandHelpMessage Build()
+        {
+            return new CommandHelpMessage(embed: _embed);
+            return new CommandHelpMessage(content: _strBuilder.ToString());
+        }
+    }
+}
