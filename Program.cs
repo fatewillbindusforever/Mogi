32d4bfdd29c4f133d4490d7ba2ad3c966b418d44
Adnan <71968681+sinnertenshi@users.noreply.github.com>
2022-02-20 17:55:22 +0200

    bot main

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+using System;
+using System.Timers;
 using System.Collections;
 using System.Linq;
 using System.Text;
@@ -14,7 +15,7 @@ namespace mei
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             MainAsync().GetAwaiter().GetResult();
         }
@@ -30,6 +31,9 @@ namespace mei
             });
 
             discord.GuildDownloadCompleted += GuildDownloadCompleted;
+            discord.GuildMemberAdded += GuildMemberAdded;
+            discord.MessageCreated += MessageCreated;
+            discord.MessageUpdated += MessageUpdated;
 
             var commands = discord.UseCommandsNext(new CommandsNextConfiguration()
             {
@@ -39,97 +43,123 @@ namespace mei
             commands.RegisterCommands<Komutlar>();
             commands.SetHelpFormatter<CustomHelpFormatter>();
 
-            discord.GuildMemberAdded += (s, e) =>
-            {
-                _ = Task.Run(async () =>
-                {
-                    var kanal = await s.GetChannelAsync(kanalid);
-                    var embed = new DiscordEmbedBuilder
-                    {
-                        Description = $"{e.Member.Mention} aramıza katıldı!",
-                    };
-                    var msg = await kanal.SendMessageAsync(embed);
-                    Console.WriteLine($"{e.Member.DisplayName}, sunucuya giriş yaptı. {DateTime.UtcNow}");
-                });
-                return Task.CompletedTask;
-            };
-
-            discord.MessageCreated += async (s, e) =>
-            {
-                if (e.Message.Content.ToLower() == "selam")
-                {
-                    await e.Message.RespondAsync("Selam!");
-                }
-                else if (e.Message.Content.ToLower() == "slm")
-                {
-                    await e.Message.RespondAsync("Selam!");
-                }
-                else if (e.Message.Content.ToLower() == "naber")
-                {
-                    await e.Message.RespondAsync("İyi senden?");
-                }
-                else if (e.Message.Content.ToLower() == "sa")
-                {
-                    await e.Message.RespondAsync("Aleyküm Selam.");
-                }
-                else if (e.Message.Content.ToLower() == "selamun aleyküm")
-                {
-                    await e.Message.RespondAsync("Aleyküm Selam!");
-                }
-                else if (e.Message.Content.ToLower() == "selamun aleykum")
-                {
-                    await e.Message.RespondAsync("Aleyküm Selam.");
-                }
-                else if (e.Message.Content.ToLower() == "ping")
-                {
-                    await e.Message.RespondAsync("pong!");
-                }
-                else if (e.Message.Content.ToLower() == "nbr")
-                {
-                    await e.Message.RespondAsync("İyi senden?");
-                }
-                else if (e.Message.Content.ToLower() == "sea")
-                {
-                    await e.Message.RespondAsync("Aleyküm Selam.");
-                }
-
-                string msg = e.Message.Content.ToLower();
-                List<string> BadWords = new List<string>()
-                {
-                    "sikerim",
-                    "siktir",
-                    "sikecem",
-                    "ibne",
-                    "amına",
-                    "orospu",
-                    "gavat",
-                    "kahpe",
-                    "piç",
-                    "fuck",
-                    "motherfucker",
-                    "porno",
-                    "porn",
-                    "sikiş",
-                    "sikişelim",
-                    "sikik",
-                    "sikis",
-                    "göt",
-                };
-                if (BadWords.Any(x => msg.Contains(x)))
-                {
-                    await e.Message.DeleteAsync();
-                    await e.Message.Channel.SendMessageAsync("Şşş küfür yok!");
-                }
-            };
-
             await discord.ConnectAsync();
             Console.WriteLine("Mei burada!");
             await Task.Delay(-1);
         }
 
-        private static async Task GuildDownloadCompleted(DiscordClient sender, GuildDownloadCompletedEventArgs e)
+        private static async Task MessageUpdated(DiscordClient s, MessageUpdateEventArgs e)
         {
-            await sender.UpdateStatusAsync(new DiscordActivity("Geliştiriliyor", ActivityType.Watching), UserStatus.Idle);
+            string msg = e.Message.Content.ToLower();
+            List<string> BadWords = new ()
+                    {
+                        "sikerim",
+                        "siktir",
+                        "sikecem",
+                        "ibne",
+                        "amına",
+                        "orospu",
+                        "gavat",
+                        "kahpe",
+                        "piç",
+                        "fuck",
+                        "motherfucker",
+                        "porno",
+                        "porn",
+                        "sikiş",
+                        "sikişelim",
+                        "sikik",
+                        "sikis",
+                        "göt",
+                    };
+
+            if (BadWords.Any(x => msg.Contains(x)))
+            {
+                await e.Message.DeleteAsync();
+                await e.Message.Channel.SendMessageAsync("Şşş küfür yok!");
+            }
+        }
+
+        private static async Task MessageCreated(DiscordClient s, MessageCreateEventArgs e)
+        {
+            if (e.Message.Content.ToLower() == "selam")
+            {
+                await e.Message.RespondAsync("Selam!");
+            }
+            else if (e.Message.Content.ToLower() == "slm")
+            {
+                await e.Message.RespondAsync("Selam!");
+            }
+            else if (e.Message.Content.ToLower() == "naber")
+            {
+                await e.Message.RespondAsync("İyi senden?");
+            }
+            else if (e.Message.Content.ToLower() == "sa")
+            {
+                await e.Message.RespondAsync("Aleyküm Selam.");
+            }
+            else if (e.Message.Content.ToLower() == "selamun aleyküm")
+            {
+                await e.Message.RespondAsync("Aleyküm Selam!");
+            }
+            else if (e.Message.Content.ToLower() == "selamun aleykum")
+            {
+                await e.Message.RespondAsync("Aleyküm Selam.");
+            }
+            else if (e.Message.Content.ToLower() == "ping")
+            {
+                await e.Message.RespondAsync("pong!");
+            }
+            else if (e.Message.Content.ToLower() == "nbr")
+            {
+                await e.Message.RespondAsync("İyi senden?");
+            }
+            else if (e.Message.Content.ToLower() == "sea")
+            {
+                await e.Message.RespondAsync("Aleyküm Selam.");
+            }
+
+            string msg = e.Message.Content.ToLower();
+            List<string> BadWords = new ()
+                    {
+                        "sikerim",
+                        "siktir",
+                        "sikecem",
+                        "ibne",
+                        "amına",
+                        "orospu",
+                        "gavat",
+                        "kahpe",
+                        "piç",
+                        "fuck",
+                        "motherfucker",
+                        "porno",
+                        "porn",
+                        "sikiş",
+                        "sikişelim",
+                        "sikik",
+                        "sikis",
+                        "göt",
+                    };
+
+            if (BadWords.Any(x => msg.Contains(x)))
+            {
+                await e.Message.DeleteAsync();
+                await e.Message.Channel.SendMessageAsync("Şşş küfür yok!");
+            }
+        }
+        private static async Task GuildMemberAdded(DiscordClient s, GuildMemberAddEventArgs e)
+        {
+            var kanal = await s.GetChannelAsync(kanalidsi);
+            var embed = new DiscordEmbedBuilder
+            {
+                Description = $"{e.Member.Mention} aramıza katıldı!",
+            };
+            await kanal.SendMessageAsync(embed);
+        }
+        private static async Task GuildDownloadCompleted(DiscordClient s, GuildDownloadCompletedEventArgs e)
+        {
+            await s.UpdateStatusAsync(new DiscordActivity("Geliştiriliyor", ActivityType.Watching), UserStatus.Idle);
         }
     }
 }
