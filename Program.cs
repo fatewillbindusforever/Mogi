617eb3f12acd610cbf678e1adb1717cc060625cb
Adnan <71968681+sinnertenshi@users.noreply.github.com>
2022-03-05 14:06:35 +0200

    Bir kaç yanıt mesajı eklendi

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace mogi
 {
     class Program
     {
-        static void Main()
+        public static void Main()
         {
             MainAsync().GetAwaiter().GetResult();
         }
@@ -24,7 +24,7 @@ namespace mogi
         {
             var discord = new DiscordClient(new DiscordConfiguration()
             {
-                Token = "bottoken",
+                Token = "TOKEN",
                 TokenType = TokenType.Bot,
                 AutoReconnect = true,
                 UseRelativeRatelimit = true,
@@ -32,9 +32,7 @@ namespace mogi
             });
 
             discord.GuildDownloadCompleted += GuildDownloadCompleted;
-            discord.GuildMemberAdded += GuildMemberAdded;
             discord.MessageCreated += MessageCreated;
-            discord.MessageUpdated += MessageUpdated;
 
             var commands = discord.UseCommandsNext(new CommandsNextConfiguration()
             {
@@ -44,58 +42,98 @@ namespace mogi
             commands.RegisterCommands<Komutlar>();
             commands.SetHelpFormatter<CustomHelpFormatter>();
 
+            discord.GuildMemberAdded += (s, e) =>
+            {
+                if(e.Guild.Id == guildid)
+                {
+                    DiscordRole gRole = e.Guild.GetRole(roleid);
+                    e.Member.GrantRoleAsync(gRole);
+                }
+                else if(e.Guild.Id == guildid)
+                {
+                    DiscordRole gRole = e.Guild.GetRole(roleid);
+                    e.Member.GrantRoleAsync(gRole);
+
+                    var wChannel = e.Guild.GetChannel(channelid);
+                    wChannel.SendMessageAsync($"{e.Member.Mention} geldiğin için teşekkürler!");
+                }
+
+                return Task.CompletedTask;
+            };
+
             await discord.ConnectAsync();
             Console.WriteLine("Mogi burada!");
             await Task.Delay(-1);
         }
-
         private static async Task MessageCreated(DiscordClient s, MessageCreateEventArgs e)
         {
-            if (e.Message.Content.ToLower() == "selam")
+            if (!e.Message.Author.IsBot)
             {
-                await e.Message.RespondAsync("Selam!");
+                if (e.Message.Content.ToLower() == "selam")
+                {
+                    await e.Message.RespondAsync("Selam!");
+                }
+                if (e.Message.Content.ToLower() == "merhaba")
+                {
+                    await e.Message.RespondAsync("Merhaba!");
+                }
+                if (e.Message.Content.ToLower() == "mrb")
+                {
+                    await e.Message.RespondAsync("Merhaba!");
+                }
+                else if (e.Message.Content.ToLower() == "slm")
+                {
+                    await e.Message.RespondAsync("Selam!");
+                }
+                else if (e.Message.Content.ToLower() == "naber")
+                {
+                    await e.Message.RespondAsync("İyi senden?");
+                }
+                else if (e.Message.Content.ToLower() == "sa")
+                {
+                    await e.Message.RespondAsync("Aleyküm Selam.");
+                }
+                else if (e.Message.Content.ToLower() == "selamun aleyküm")
+                {
+                    await e.Message.RespondAsync("Aleyküm Selam!");
+                }
+                else if (e.Message.Content.ToLower() == "selamun aleykum")
+                {
+                    await e.Message.RespondAsync("Aleyküm Selam.");
+                }
+                else if (e.Message.Content.ToLower() == "ping")
+                {
+                    await e.Message.RespondAsync("pong!");
+                }
+                else if (e.Message.Content.ToLower() == "nbr")
+                {
+                    await e.Message.RespondAsync("İyi senden?");
+                }
+                else if (e.Message.Content.ToLower() == "sea")
+                {
+                    await e.Message.RespondAsync("Aleyküm Selam.");
+                }
+                /*else if (e.Message.Content.ToLower() == "bumptest")
+                {
+                    if (e.Author.Id == authorid)
+                    {
+                        var bRole = e.Guild.GetRole(roleid);
+                        await e.Channel.SendMessageAsync($"Bumplaaa! {bRole.Mention}");
+                    }
+                }*/
+                else if (e.Message.Content.ToLower() == "mogi")
+                {
+                    string[] mMsg = {
+                    "Merhaba, bana mı seslendiniz?",
+                    "Size nasıl yardımcı olabilirim?",
+                    "Yardıma ihtiyacınız varsa !help komutunu kullanabilirsiniz."
+                    };
+                    
+                    Random rand = new Random();
+                    int send = rand.Next(mMsg.Length);
+                    await e.Channel.SendMessageAsync($"{mMsg[send]}");
+                }
             }
-            else if (e.Message.Content.ToLower() == "slm")
-            {
-                await e.Message.RespondAsync("Selam!");
-            }
-            else if (e.Message.Content.ToLower() == "naber")
-            {
-                await e.Message.RespondAsync("İyi senden?");
-            }
-            else if (e.Message.Content.ToLower() == "sa")
-            {
-                await e.Message.RespondAsync("Aleyküm Selam.");
-            }
-            else if (e.Message.Content.ToLower() == "selamun aleyküm")
-            {
-                await e.Message.RespondAsync("Aleyküm Selam!");
-            }
-            else if (e.Message.Content.ToLower() == "selamun aleykum")
-            {
-                await e.Message.RespondAsync("Aleyküm Selam.");
-            }
-            else if (e.Message.Content.ToLower() == "ping")
-            {
-                await e.Message.RespondAsync("pong!");
-            }
-            else if (e.Message.Content.ToLower() == "nbr")
-            {
-                await e.Message.RespondAsync("İyi senden?");
-            }
-            else if (e.Message.Content.ToLower() == "sea")
-            {
-                await e.Message.RespondAsync("Aleyküm Selam.");
-            }
-        }
-        private static async Task GuildMemberAdded(DiscordClient s, GuildMemberAddEventArgs e)
-        {
-            var kanal = await s.GetChannelAsync(channelid);
-            var embed = new DiscordEmbedBuilder
-            {
-                Description = $"{e.Member.Mention} aramıza katıldı!",
-            };
-            await kanal.SendMessageAsync(embed);
         }
         private static async Task GuildDownloadCompleted(DiscordClient s, GuildDownloadCompletedEventArgs e)
         {
